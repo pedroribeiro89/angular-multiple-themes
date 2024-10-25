af7a8eef6cf63d9ab68d802bca27d050d166852d
PedroRibeiro <pedroribeiro89@gmail.com>
2024-10-25 17:42:22 -0300

    update angular material15

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatLegacyButtonModule as MatButtonModule} from "@angular/material/legacy-button";
 import {MatToolbarModule} from "@angular/material/toolbar";
 import {MatIconModule} from "@angular/material/icon";
 import { ThemeExampleComponent } from './theme-example/theme-example.component';
 import { MaterialExampleComponent } from './material-example/material-example.component';
 import { EnvExampleComponent } from './env-example/env-example.component';
 import {MatDividerModule} from "@angular/material/divider";
-import {MatLegacyFormFieldModule as MatFormFieldModule} from "@angular/material/legacy-form-field";
-import {MatLegacyInputModule as MatInputModule} from "@angular/material/legacy-input";
+import {MatButtonModule} from "@angular/material/button";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
 
 @NgModule({
   declarations: [
